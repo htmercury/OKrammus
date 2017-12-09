77389c1db7a35d5fc15f684a4b57dbcbded95b17
Ka Wong <kawong2020@u.northwestern.edu>
2017-12-09 04:42:17 -0600

    Initial test files with mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 ﻿var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/OKrammusModel'), //created model loading here
+    bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/OKrammusdb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/OKrammusRoutes'); //importing route
+routes(app); //register the route
+
 
 app.listen(port);
 
+
 console.log('todo list RESTful API server started on: ' + port);
+
+app.use(function (req, res) {
+    res.status(404).send({ url: req.originalUrl + ' not found' })
+});
