c517783f50a546b70dc8959b783d26c555412884
Ka Wong <kawong2020@u.northwestern.edu>
2017-12-12 03:50:47 -0600

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 8080,
     mongoose = require('mongoose'),
     Champions = require('./api/models/OKrammusModel'), //created model loading here
     bodyParser = require('body-parser');
