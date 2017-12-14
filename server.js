0538796be4981892547cd024c5c5e14311c13ef5
Ka Wong <kawong2020@u.northwestern.edu>
2017-12-13 21:35:55 -0600

    Slight fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express'),
     app = express(),
-    port = process.env.PORT || 8080,
+    port = process.env.PORT || 3000,
     mongoose = require('mongoose'),
     Champions = require('./api/models/OKrammusModel'), //created model loading here
     bodyParser = require('body-parser');
